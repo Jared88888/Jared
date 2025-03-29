692ab62c873f499f5d35a2ee338b78cbcf9475a4
Jared88888 <jaredseow432@gmail.com>
2025-03-29 07:29:48 +0000

    29 / 3 / 2025
    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

--- u09_formatcheck_validation/u9_formatcheck_validation.py
+++ u09_formatcheck_validation/u9_formatcheck_validation.py
@@ -183,9 +183,81 @@
 
 # Input: "Hello, World! Python Is Amazing."
 # Output: "Number of uppercase letters: 4"
+# sentence = input("Enter a sentence: ")
+# upper = 0
+# for i in sentence:
+#     if i.isupper():
+#         upper += 1
+# print(f"Theere are {upper} uppercase letters")
+
+
+
+
+#################################################
+# Assignment 1: Password Validation
+# Scenario: Validate a password to ensure it is at least 8 characters long and contains:
+# # - At least one uppercase letter.
+# # - At least one lowercase letter.
+# # - At least one digit.
+
+# # Example:
+# # Input: "Secure123"
+# # Output: "Valid password"
+
+# # Input: "password"
+# # Output: "Invalid password: missing uppercase letter and digit"
+
+# # Input: "PASSWORD123"
+# # Output: "Invalid password: missing lowercase letter"
+
+# password = input("Type a password: ")
+
+# upperok = False
+# lowerok = False
+# digitok = False
+
+# for i in password:
+#     if i.isupper() == True and upperok == False:
+#         upperok = True
+#     elif i.islower() == True and lowerok == False:
+#         lowerok = True
+#     elif i.isdigit() == True and digitok == False:
+#         digitok = True
+
+# if upperok == True and lowerok == True and digitok == True and len(password) >= 8:
+#     print(f"Password ok")
+# else:
+#     print(f"Password not ok")
+#     print(f"Upper Case is OK = {upperok}")
+#     print(f"Lower Case is OK = {lowerok}")
+#     print(f"Digit is OK = {digitok}")
+#     print(f"Length is OK = {len(password) > 8}")
+
+
+##################################################################
+# Scenario: Ensure a sentence starts with a capital letter and ends with a period.
+
+# Example:
+# Input: "The quick brown fox jumps over the lazy dog."
+# Output: "Valid sentence"
+
+# Input: "the quick brown fox jumps over the lazy dog"
+# Output: "Invalid sentence: does not start with a capital letter and does not end with a period"
+
+# Input: "Hello world"
+# Output: "Invalid sentence: does not end with a period"
+ 
 sentence = input("Enter a sentence: ")
-upper = 0
-for i in sentence:
-    if i.isupper():
-        upper += 1
-print(f"Theere are {upper} uppercase letters")
+length = len(sentence)
+lastcharacter = sentence[length - 1]
+firstcharacter = sentence[0]
+
+# last = sentence[-1]
+# print(last) #alternate
+
+if firstcharacter.isupper() and lastcharacter == ".":
+    print("Valid")
+else:
+    print("Invalid")
+
+    
